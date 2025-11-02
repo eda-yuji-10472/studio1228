133ab1c847b01050d5e8543b24e580485e16cccc
江田雄二 <eda.yuji.10472@colopl.co.jp>
2025-11-02 05:47:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7105256715-9dfda",
+  "appId": "1:573695013169:web:61fa161aa55339f0cc8010",
+  "apiKey": "AIzaSyCOzoziAjTfVyQGrCXScjt0D28MY22Fyto",
+  "authDomain": "studio-7105256715-9dfda.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "573695013169"
+};
